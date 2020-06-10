d73de4bfa34fd14c34ad87545757731fa0b31264
gauransh-soni <gauranshiitd@gmail.com>
2020-06-10 15:22:22 +0530

    asasd

---
+++ ishare-main/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Danger from './Danger';
+
+function App() {
+  return (
+    <div className="App">
+      <Danger></Danger>
+    </div>
+  );
+}
+
+export default App;
---
+++ ishare-main/src/Danger.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+
+const Danger= (props) => {
+  return (
+    <Button color="danger">Danger!</Button>
+  );
+};
+
+export default Danger
